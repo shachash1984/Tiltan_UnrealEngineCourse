2199b724a9d6a1a8284c3ea4911d1b515336d6c0
Shahar Schneider <shachash1984@gmail.com>
2022-10-31 16:05:27 +0200

    Added more materials for class 1

---
+++ Classes/Class_1/CPP_HelloWorld/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World\n";
+	
+	std::cout << "Please enter your age:\n";
+	int age;
+	std::cin >> age;
+	
+	std::cout << "Please enter your name:\n";
+	std::string name;
+	std::cin >> name;
+	std::cout << "My name is " << name << " and I am " << age << " years old.\n";
+	
+	float HP = -100.30f;
+	
+	char end;
+	std::cin >> end;
+	return 0;
+}
+
+//Fundamental types:
+//int, float, double, char, bool, void
+
+//Fundamental types extended:
+//long, short, long long, unsigned, unsigned long, unsigned short, unsigned long long
+
+// ASCII values
+//int main()
+//{
+//    std::cout << "Printable ASCII [32..126]:\n";
+//    for (char c{' '}; c <= '~'; c++) {
+//        std::cout << c << ((c+1) % 32 ? ' ' : '\n');
+//    }
+//}
