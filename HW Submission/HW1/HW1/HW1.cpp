2ccd499a6d71ffa78bcb2821527f1557e37d1ef9
Mark <mark489@live.ru>
2022-11-07 01:32:24 +0200

    First C++ Program

---
+++ HW Submission/HW1/HW1/HW1.cpp
@@ -0,0 +1,21 @@
+// HW1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+using namespace std;
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	string name;
+	int age, points = 100;
+	float HP, mana;
+	unsigned char strength, magic, dexterity, vitality;
+	
+	cout << "Welcome Player! Please enter your name: ";
+	cin >> name;
+	cout << "Hello " << name;
+	cout << " How old are you ? ";
+	cin >> age;
+	cout << "Excellent ! you have 100 points to create your character";
+}
