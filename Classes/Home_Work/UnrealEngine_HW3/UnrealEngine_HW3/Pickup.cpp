d4cc8fe02dc57f3e280f239bf8d4e90e8875b0bc
Shahar Schneider <19328796+shachash1984@users.noreply.github.com>
2022-11-19 03:34:42 +0200

    Checked assignments 1 and 3 (CPP only - no UE)

--- Classes/Home_Work/UnrealEngine_HW3/UnrealEngine_HW3/Pickup.cpp
+++ Classes/Home_Work/UnrealEngine_HW3/UnrealEngine_HW3/Pickup.cpp
@@ -5,17 +5,18 @@ using std::to_string;
 
 Pickup::Pickup() : name("Health")
 {
-	position = Point();
-	value = 0;
+	position = Point(); // No need for this line, the default ctor will do it for you
+	value = 0; //Again, should be initialized in the ctor's initialization list -1 pt
 }
 
-Pickup::Pickup(Point newPos, string newName, int newValue) : position(newPos), name(newName), value(newValue)
+Pickup::Pickup(Point newPos, string newName, int newValue) : position(newPos), name(newName), value(newValue) //This is good! although we have not learned copy ctor yet (for Point)
 {
-	position = newPos;
-	value = newValue;
+	position = newPos; //Why do it again? -1 pt
+	value = newValue;	//Why do it again?
 }
 
 string Pickup::ToString()
 {
-	return std::format("Pickup: name: {}, position:({}, {}, {}), value: {}", name, position.GetX(), position.GetY(), position.GetZ(), value);
+	return std::format("Pickup: name: {}, position:({}, {}, {}), value: {}", name, position.GetX(), position.GetY(), position.GetZ(), value); //We haven't learned this yet, but it's good to see you're trying to use it.
+																																				//it is in a more modern version of CPP - did it compile for you?
 }
--- Classes/Home_Work/UnrealEngine_HW3/UnrealEngine_HW3/Point.cpp
+++ Classes/Home_Work/UnrealEngine_HW3/UnrealEngine_HW3/Point.cpp
@@ -7,3 +7,8 @@ Point::Point()
 	y = 0;
 	z = 0;
 }
+
+//Point::Point() : x(0), y(0), z(0)		// This is how the default ctor should look like
+//										// You need to initialize the variables and not assign them. -5 pts
+//{
+//}
