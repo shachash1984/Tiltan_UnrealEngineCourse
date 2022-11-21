d87b82a3a3ab2b2832006261ca9dc06745f6f4e2
RonTheDragon <gweta.ron@gmail.com>
2022-11-22 00:14:18 +0200

    CPP3 homework fix

--- Ron Guetta/CPP homework 3.cpp
+++ Ron Guetta/CPP homework 3.cpp
@@ -6,7 +6,7 @@ struct Point {
 	int x;
 	int y;
 	int z;
-	Point() {};
+	Point();
 };
 
 class Pickup {
@@ -14,21 +14,13 @@ class Pickup {
 	string const name;
 	int value;
 public:
-	Pickup() : name("") { position.x = 0; position.y = 0, position.z = 0, value = 0; };
-	Pickup(Point position, string Name, int value) : name(Name)
-	{
-		this->position = position;
-		this->value = value;
-	}
-	Point GetPosition() { return position; };
-	string GetName() { return name; };
-	int GetValue() { return value; };
-	string ToString() { 
-		string xx = to_string(position.x);
-		string yy = to_string(position.y);
-		string zz = to_string(position.z);
-		string val = to_string(value);
-		return "Pickup: " + name + ":Health, position:(" +xx+","+yy+","+zz+"), value: "+ val+"\n"; }
+	Pickup();
+	Pickup(Point position, string name, int value);
+
+	Point GetPosition();
+	string GetName();
+	int GetValue();
+	string ToString();
 };
 
 
@@ -43,3 +35,30 @@ int main()
 	cout << pickup.ToString();
 	cout << pick.ToString();
 }
+
+ Point::Point() : x(0), y(0), z(0) {};
+
+ Pickup::Pickup() : name(""), value(0) {};
+
+ Pickup::Pickup(Point position, string name, int value) : name(name), value(value)
+ {
+	 this->position.x = position.x;
+	 this->position.y = position.y;
+	 this->position.z = position.z;
+ };
+
+ Point Pickup::GetPosition() { return position; };
+
+ string Pickup::GetName() { return name; };
+
+ int Pickup::GetValue() { return value; };
+
+ string Pickup::ToString() 
+ {
+	 string xx = to_string(position.x);
+	 string yy = to_string(position.y);
+	 string zz = to_string(position.z);
+	 string val = to_string(value);
+	 return "Pickup: " + name + ":Health, position:(" + xx + "," + yy + "," + zz + "), value: " + val + "\n";
+ }
+ 
