484bc7fa350f5ee9b1e46a5b2171e6b6ade5f781
RonTheDragon <gweta.ron@gmail.com>
2022-11-25 17:59:45 +0200

    everything bad

--- FirstCppProject/Intermediate/Build/Win64/FirstCppProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.34.31933
-#include "G:/Unreal Projects/Tiltan_UnrealEngineCourse/FirstCppProject/Intermediate/Build/Win64/FirstCppProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- FirstCppProject/Source/FirstCppProject/FirstCppProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FirstCppProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstCppProject, "FirstCppProject" );
