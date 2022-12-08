5bc0bd81c5e7882d43f8b22f9ce8612dc52d58a2
ShaiMaytav <shaimaytav01@gmail.com>
2022-12-08 20:29:47 +0200

    Class6 HW done

---
+++ HW/Class456HW/Intermediate/Build/Win64/UnrealEditor/Inc/Class4HW/Target2.generated.h
@@ -0,0 +1,90 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CLASS4HW_Target2_generated_h
+#error "Target2.generated.h already included, missing '#pragma once' in Target2.h"
+#endif
+#define CLASS4HW_Target2_generated_h
+
+#define FID_Class456HW_Source_Class4HW_Public_Target2_h_12_SPARSE_DATA
+#define FID_Class456HW_Source_Class4HW_Public_Target2_h_12_RPC_WRAPPERS
+#define FID_Class456HW_Source_Class4HW_Public_Target2_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Class456HW_Source_Class4HW_Public_Target2_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATarget2(); \
+	friend struct Z_Construct_UClass_ATarget2_Statics; \
+public: \
+	DECLARE_CLASS(ATarget2, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Class4HW"), NO_API) \
+	DECLARE_SERIALIZER(ATarget2)
+
+
+#define FID_Class456HW_Source_Class4HW_Public_Target2_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesATarget2(); \
+	friend struct Z_Construct_UClass_ATarget2_Statics; \
+public: \
+	DECLARE_CLASS(ATarget2, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Class4HW"), NO_API) \
+	DECLARE_SERIALIZER(ATarget2)
+
+
+#define FID_Class456HW_Source_Class4HW_Public_Target2_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATarget2(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATarget2) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATarget2); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATarget2); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATarget2(ATarget2&&); \
+	NO_API ATarget2(const ATarget2&); \
+public:
+
+
+#define FID_Class456HW_Source_Class4HW_Public_Target2_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATarget2(ATarget2&&); \
+	NO_API ATarget2(const ATarget2&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATarget2); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATarget2); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATarget2)
+
+
+#define FID_Class456HW_Source_Class4HW_Public_Target2_h_9_PROLOG
+#define FID_Class456HW_Source_Class4HW_Public_Target2_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Class456HW_Source_Class4HW_Public_Target2_h_12_SPARSE_DATA \
+	FID_Class456HW_Source_Class4HW_Public_Target2_h_12_RPC_WRAPPERS \
+	FID_Class456HW_Source_Class4HW_Public_Target2_h_12_INCLASS \
+	FID_Class456HW_Source_Class4HW_Public_Target2_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Class456HW_Source_Class4HW_Public_Target2_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Class456HW_Source_Class4HW_Public_Target2_h_12_SPARSE_DATA \
+	FID_Class456HW_Source_Class4HW_Public_Target2_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Class456HW_Source_Class4HW_Public_Target2_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Class456HW_Source_Class4HW_Public_Target2_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CLASS4HW_API UClass* StaticClass<class ATarget2>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Class456HW_Source_Class4HW_Public_Target2_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ HW/Class456HW/Source/Class4HW/Private/Target.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Target.h"
+#include "Class4HW\Class4HW.h"
+#include "UObject/ConstructorHelpers.h"
+
+// Sets default values
+ATarget::ATarget()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	if (!meshComponent)
+	{
+		meshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+		static ConstructorHelpers::FObjectFinder<UStaticMesh>Mesh(TEXT("'/Game/Meshes/SM_Target.SM_Target'"));
+		if (Mesh.Succeeded())
+		{
+			meshComponent->SetStaticMesh(Mesh.Object);
+		}
+
+		meshComponent->SetupAttachment(RootComponent);
+	}
+
+}
+
+// Called when the game starts or when spawned
+void ATarget::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+}
+
+void ATarget::OnHit()
+{
+	UE_LOG(LogClass4HW, Log, TEXT("Tar Coll"));
+	Destroy();
+}
+
