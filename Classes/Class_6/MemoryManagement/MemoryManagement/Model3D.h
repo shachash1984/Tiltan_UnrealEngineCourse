81a6912a4164a4679a6460433e800b4aec523753
DSlayer <93089045+sharon123454@users.noreply.github.com>
2022-12-12 22:09:12 +0200

    Revert "Revert "Merge branch 'main' into Sharon-Vishnivetsky""
    This reverts commit 92a36ffc833f9b891df979dcf3bfa76b2a6b0053.

---
+++ Classes/Class_6/MemoryManagement/MemoryManagement/Model3D.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+
+class Model3D
+{
+	
+public:
+	Model3D();
+	Model3D(std::string name);
+	~Model3D();
+
+	std::string GetName() const {return Name;} 
+
+
+private:
+	std::string Name;
+};
+
