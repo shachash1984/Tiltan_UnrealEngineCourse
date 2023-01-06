ec4c1f5e7a07fce02477de438e15b6961be5de9a
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2023-01-06 14:47:17 +0200

    Added Old CPP_HW3 that I did with Shay Sedaka

---
+++ HW/CPP_HW3/Pickup.cpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "Pickup.h"
+
+// This HW was made with Shay Sedaka 
+
+std::string Pickup::ToString()
+{
+	std::string strToReturn = "Name: " + name + ", position: " + position.ToString() + ", value: " + std::to_string(value);
+	return strToReturn;
+}
+
+Pickup::Pickup(Point pos, std::string name, int value) {
+	this->position = pos;
+	this->name = name;
+	this->value = value;
+}
---
+++ HW/CPP_HW3/Pickup.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include "Point.h"
+
+// This HW was made with Shay Sedaka 
+
+
+class Pickup{
+
+private:
+	Point position;
+	std::string name; // we removed the const in order to be able to write a ctor for this class
+	int value;
+
+public:
+	Pickup(); // default ctor
+	Pickup(Point pos, std::string name, int value);
+
+	Point GetPosition() { return position; }
+	std::string GetName() { return name; }
+	int GetValue() { return value; }
+	
+	std::string ToString();
+};
+
+
