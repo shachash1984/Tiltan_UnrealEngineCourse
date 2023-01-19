c5b5c337f235dc6c3c859eae1a0e890cff3a0edb
RonTheDragon <gweta.ron@gmail.com>
2023-01-19 02:24:25 +0200

    fixed homework

--- HW/CPP Homework 12/Question1.cpp
+++ HW/CPP Homework 12/Question1.cpp
@@ -1,4 +1,5 @@
 #include "Question1.h"
+#include <algorithm>
 
 void Question1::PrintSumPairs1(std::vector<int> vec, int num)
 {
@@ -17,30 +18,52 @@ void Question1::PrintSumPairs1(std::vector<int> vec, int num)
 	}
 }
 
-void Question1::PrintSumPairs2(std::vector<int> vec, int num)
+//void Question1::PrintSumPairs2(std::vector<int> vec, int num)
+//{
+//	float n = vec.capacity();
+//	float count = n * (n + 1) / 2;
+//
+//	int i = 0;
+//	int j = 0;
+//	for (size_t x = 0; x < count; x++)
+//	{
+//		if (i != j)
+//		{
+//			if (vec[j] + vec[i] == num)
+//			{
+//				std::cout << vec[i] << "+" << vec[j] << "=" << num << "\n";
+//			}
+//		}
+//
+//		j++;
+//		if (j == n) 
+//		{
+//			i++;
+//			j = i;			
+//		}
+//	}
+//}
+//n*(n+1)/2
+
+void Question1::PrintSumPairs2(std::vector<int> vec, int num) // fix after critics
 {
-	float n = vec.capacity();
-	float count = n * (n + 1) / 2;
+	std::sort(vec.begin(), vec.end());				// sorting smallest to biggest
 
-	int i = 0;
-	int j = 0;
-	for (size_t x = 0; x < count; x++)
+	int left = 0, right = vec.size() - 1;			// left is smallest, right is biggest
+
+	while (left < right)							// left must always be smaller
 	{
-		if (i != j)
+		if (vec[left] + vec[right] == num)			// if they equal then right them down
 		{
-			if (vec[j] + vec[i] == num)
-			{
-				std::cout << vec[i] << "+" << vec[j] << "=" << num << "\n";
-			}
+			std::cout << vec[left] << "+" << vec[right] << "=" << num << "\n";
+			left++;									// if the sum of the numbers is equal to the wanted number, both numbers wont be usefull again
+			right--;
 		}
 
-		j++;
-		if (j == n) 
-		{
-			i++;
-			j = i;			
-		}
+		else if (vec[left] + vec[right] < num)		// if the sum is too small then we should make the smaller number bigger
+			left++;
+
+		else										// if the sum is too big then we should make the bigger number smaller
+			right--;
 	}
 }
-
-//n*(n+1)/2
