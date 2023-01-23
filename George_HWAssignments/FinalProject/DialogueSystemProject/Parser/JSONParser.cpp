d6a4fb9ef397c0306dc895602abaf07cb81e640a
George Rashed <goerge1110@gmail.com>
2023-01-23 21:45:39 +0200

    Prepping Final Project.

---
+++ George_HWAssignments/FinalProject/DialogueSystemProject/Parser/JSONParser.cpp
@@ -0,0 +1,29 @@
+#include "JSONParser.h"
+
+void JSONParser::SetIndexFilePath(const std::string& _indexFilePath)
+{
+}
+
+const std::string& JSONParser::GetIndexFilePath() const
+{
+	// TODO: insert return statement here
+}
+
+std::vector<std::string> JSONParser::GetData()
+{
+	return std::vector<std::string>();
+}
+
+bool JSONParser::Parse()
+{
+	return false;
+}
+
+bool JSONParser::OpenFile(const std::string& FilePath)
+{
+	return false;
+}
+
+void JSONParser::CloseFile()
+{
+}
