f3dfbf398848977fdd22f1d6f9cc3f925ef1d1c4
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2023-01-23 23:35:10 +0200

    Start of Final Project - work with Shachar in class
    Watch the recording

---
+++ FinalProject/DialogApp/Dialog/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "JSONParser.h"
+
+int main()
+{
+	using namespace std;
+
+	/*JSONParser parser;*/
+	cout << "Hello World" << endl;
+	return 0;
+}
---
+++ FinalProject/DialogApp/Parser/JSONParser.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "json/json.h"
+#include "IParser.h"
+#include <fstream>
+
+class JSONParser : public IParser
+{
+public:
+	void SetIndexFilePath(const std::string& _indexFilePath);
+	const std::string& GetIndexFilePath() const;
+
+	std::vector<std::string> GetData() override;
+
+	bool Parse();
+	Json::Value& GetRoot() { return root; }
+	bool OpenFile(const std::string& FilePath);
+	void CloseFile();
+
+protected:
+	std::string indexFilePath;
+	Json::Reader reader;
+	Json::Value root;   // 'root' will contain the root value after parsing.
+	std::ifstream inputFile;
+};
