88fc488556fcc99f70ed0b4ec336b31659be994a
ShaiMaytav <shaimaytav01@gmail.com>
2023-01-29 17:27:59 +0200

    project base done

--- HW/DialogueApp/Parser/JSONParser.cpp
+++ HW/DialogueApp/Parser/JSONParser.cpp
@@ -1,1 +1,29 @@
 #include "JSONParser.h"
+
+void JSONParser::SetIndexFilePath(const std::string& _indexFilePath)
+{
+}
+
+const std::string& JSONParser::GetIndexFilePath() const
+{
+	return "Text";
+}
+
+std::vector<std::string> JSONParser::GetData()
+{
+	return std::vector<std::string>();
+}
+
+bool JSONParser::Parse()
+{
+	return false;
+}
+
+bool JSONParser::OpenFile(const std::string& FilePath)
+{
+	return false;
+}
+
+void JSONParser::CloseFile()
+{
+}
