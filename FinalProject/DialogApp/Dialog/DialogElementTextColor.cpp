c7d0d513cfd59f41487490610a01fbe20e5f0494
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2023-02-03 23:31:50 +0200

    I AM ALLOWED TO PUSH THESE WEIRD FILES! DOBY IS FREEEEEE!

---
+++ FinalProject/DialogApp/Dialog/DialogElementTextColor.cpp
@@ -0,0 +1,13 @@
+#include "DialogElementTextColor.h"
+
+DialogElementTextColor::DialogElementTextColor(std::shared_ptr<IGUI> const& pGui, IGUI::Color _color) : IDialogCreationElement(pGui)
+{
+}
+
+DialogElementTextColor::~DialogElementTextColor()
+{
+}
+
+void DialogElementTextColor::Act()
+{
+}
---
+++ FinalProject/DialogApp/Dialog/DialogElementWindowColor.cpp
@@ -0,0 +1,13 @@
+#include "DialogElementWindowColor.h"
+
+DialogElementWindowColor::DialogElementWindowColor(std::shared_ptr<IGUI> const& pGui, IGUI::Color _color) : IDialogCreationElement(pGui)
+{
+}
+
+DialogElementWindowColor::~DialogElementWindowColor()
+{
+}
+
+void DialogElementWindowColor::Act()
+{
+}
