4e4930b30b19508b225d309fe5ff4c8d45669e08
shaya14 <84465665+shaya14@users.noreply.github.com>
2023-02-08 03:43:58 +0200

    Class 9

--- HW/Class 9/Class 9/Class 9/Main.cpp
+++ HW/Class 9/Class 9/Class 9/Main.cpp
@@ -10,15 +10,23 @@ int main()
 {
 	SpaceShip ship("The Flying Dutchman", std::vector<Weapon*>());
 
-	Gun gun("Pistol", 10, 100, 1, 100);
-	Cannon cannon("Cannon", 100, 1000, 10, 100);
-	Laser laser("Laser", 1000, 10000, 100, 100);
+	Gun gun("pistol", 10, 100, 1, 100);
+	Cannon cannon("cannon", 100, 1000, 10, 100);
+	Laser laser("laser", 1000, 10000, 100, 100);
+
+	//Weapon* gun = new Gun("pistol", 10, 100, 1, 100);
+	//Weapon* cannon = new Cannon("cannon", 100, 1000, 10, 100);
+	//Weapon* laser = new Laser("laser", 1000, 10000, 100, 100);
 
 	ship.AddWeapon(gun);
 	ship.AddWeapon(cannon);
 	ship.AddWeapon(laser);
 
 	ship.FireEverythingWeGot();
+
+	//delete gun;
+	//delete cannon;
+	//delete laser;
 
 	return 0;
 }
