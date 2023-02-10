0f531718a9413447fb7925a8d05650f99deb56ec
RonTheDragon <gweta.ron@gmail.com>
2023-02-10 08:50:46 +0200

    Project Done

--- FinalProject/MyFinalProject/DialogApp/Dialog/main.cpp
+++ FinalProject/MyFinalProject/DialogApp/Dialog/main.cpp
@@ -50,99 +50,102 @@ int main()
 
 #pragma region with JSONParser
 	
-	parser.SetIndexFilePath("Dialogs\\DialogIndex.json");
+	parser.SetIndexFilePath("Dialogs\\MyDialogIndex.json");
 	if (parser.OpenFile(parser.GetIndexFilePath()))
 	{
 		if (parser.Parse())
 		{
 		for (int i = 0; i < parser.GetData().size(); i++)
 		{
-			Json::Value tmpRoot;
-			//ifstream thisFile(files[i]);
-			ifstream thisFile(parser.GetRoot().get(parser.GetData()[i], "Error").asString());
-			if (thisFile.is_open()) {
-				reader.parse(thisFile, tmpRoot);
+			JSONParser tempParser;
+			
+			if (tempParser.OpenFile(parser.GetRoot().get(parser.GetData()[i], "Error").asString())) 
+			{
+				if (tempParser.Parse()) {
+					//reader.parse(thisFile, tempParser.GetRoot());
 
-				auto box = std::make_shared<DialogBoxBase>();
+					auto box = std::make_shared<DialogBoxBase>();
 
-				// Title
-				//DialogElementTitle d(gui,"");
-				box->RegisterElementToOnRender(std::make_shared<DialogElementTitle>
-					(gui, tmpRoot.get("title", "").asString()));
+					// Title
+					//DialogElementTitle d(gui,"");
+					box->RegisterElementToOnRender(std::make_shared<DialogElementTitle>
+						(gui, tempParser.GetRoot().get("title", "").asString()));
 
-				// Position
-				//DialogElementPosition d(gui, point);
-				IGUI::Point point;
-				point.x = tmpRoot.get("position", 0).get("x", 0).asInt();
-				point.y = tmpRoot.get("position", 0).get("y", 0).asInt();
-				box->RegisterElementToOnCreate(std::make_shared<DialogElementPosition>
-					(gui, point));
+					// Position
+					//DialogElementPosition d(gui, point);
+					IGUI::Point point;
+					point.x = tempParser.GetRoot().get("position", 0).get("x", 0).asInt();
+					point.y = tempParser.GetRoot().get("position", 0).get("y", 0).asInt();
+					box->RegisterElementToOnCreate(std::make_shared<DialogElementPosition>
+						(gui, point));
 
-				// Size
-				//DialogElementSize d(gui, 0, 0);
-				box->RegisterElementToOnCreate(std::make_shared<DialogElementSize>
-					(gui,
-						tmpRoot.get("size", 0).get("width", 0).asInt(),
-						tmpRoot.get("size", 0).get("height", 0).asInt()
-						)
-				);
-
-				// Text
-				const Json::Value& text = tmpRoot["text"];
-				vector<string> textVector(text.size());
-				for (unsigned int index = 0; index < text.size(); ++index)
-				{
-					textVector[index] = text[index].asString();
-				}
-				//DialogElementText d(gui, textVector);
-				box->RegisterElementToOnRender(std::make_shared<DialogElementText>
-					(gui,
-						textVector
-						)
-				);
-
-				// WindowColor
-				IGUI::Color windowColor;
-				windowColor.r = tmpRoot.get("window_color", 0).get("r", 0).asFloat();
-				windowColor.g = tmpRoot.get("window_color", 0).get("g", 0).asFloat();
-				windowColor.b = tmpRoot.get("window_color", 0).get("b", 0).asFloat();
-				windowColor.a = tmpRoot.get("window_color", 0).get("a", 0).asFloat();
-				//DialogElementWindowColor d(gui, windowColor);
-				box->RegisterElementToOnCreate(std::make_shared<DialogElementWindowColor>
-					(gui,
-						windowColor)
-				);
-
-				// WindowColor
-				IGUI::Color textColor;
-				textColor.r = tmpRoot.get("text_color", 0).get("r", 0).asFloat();
-				textColor.g = tmpRoot.get("text_color", 0).get("g", 0).asFloat();
-				textColor.b = tmpRoot.get("text_color", 0).get("b", 0).asFloat();
-				textColor.a = tmpRoot.get("text_color", 0).get("a", 0).asFloat();
-				//DialogElementTextColor d(gui, textColor);
-				box->RegisterElementToOnCreate(std::make_shared<DialogElementTextColor>
-					(gui,
-						textColor)
-				);
-
-				// Buttons 
-				const Json::Value& buttons = tmpRoot["buttons"];
-				//DialogElementButton d(gui, "", 0);
-				for (unsigned int index = 0; index < buttons.size(); ++index)
-				{
-					box->RegisterElementToOnButton(std::make_shared<DialogElementButton>
+					// Size
+					//DialogElementSize d(gui, 0, 0);
+					box->RegisterElementToOnCreate(std::make_shared<DialogElementSize>
 						(gui,
-							buttons[index].get("text", "").asString(),
-							buttons[index].get("next", "").asInt()
+							tempParser.GetRoot().get("size", 0).get("width", 0).asInt(),
+							tempParser.GetRoot().get("size", 0).get("height", 0).asInt()
 							)
 					);
+
+					// Text
+					const Json::Value& text = tempParser.GetRoot()["text"];
+					vector<string> textVector(text.size());
+					for (unsigned int index = 0; index < text.size(); ++index)
+					{
+						textVector[index] = text[index].asString();
+					}
+					//DialogElementText d(gui, textVector);
+					box->RegisterElementToOnRender(std::make_shared<DialogElementText>
+						(gui,
+							textVector
+							)
+					);
+
+					// WindowColor
+					IGUI::Color windowColor;
+					windowColor.r = tempParser.GetRoot().get("window_color", 0).get("r", 0).asFloat();
+					windowColor.g = tempParser.GetRoot().get("window_color", 0).get("g", 0).asFloat();
+					windowColor.b = tempParser.GetRoot().get("window_color", 0).get("b", 0).asFloat();
+					windowColor.a = tempParser.GetRoot().get("window_color", 0).get("a", 0).asFloat();
+					//DialogElementWindowColor d(gui, windowColor);
+					box->RegisterElementToOnCreate(std::make_shared<DialogElementWindowColor>
+						(gui,
+							windowColor)
+					);
+
+					// WindowColor
+					IGUI::Color textColor;
+					textColor.r = tempParser.GetRoot().get("text_color", 0).get("r", 0).asFloat();
+					textColor.g = tempParser.GetRoot().get("text_color", 0).get("g", 0).asFloat();
+					textColor.b = tempParser.GetRoot().get("text_color", 0).get("b", 0).asFloat();
+					textColor.a = tempParser.GetRoot().get("text_color", 0).get("a", 0).asFloat();
+					//DialogElementTextColor d(gui, textColor);
+					box->RegisterElementToOnCreate(std::make_shared<DialogElementTextColor>
+						(gui,
+							textColor)
+					);
+
+					// Buttons 
+					const Json::Value& buttons = tempParser.GetRoot()["buttons"];
+					//DialogElementButton d(gui, "", 0);
+					for (unsigned int index = 0; index < buttons.size(); ++index)
+					{
+						box->RegisterElementToOnButton(std::make_shared<DialogElementButton>
+							(gui,
+								buttons[index].get("text", "").asString(),
+								buttons[index].get("next", "").asInt()
+								)
+						);
+					}
+
+
+
+					gui->AddDialogBox(tempParser.GetRoot().get("id", -1).asInt(), box);
+
+					tempParser.CloseFile();
+					//thisFile.close();
 				}
-
-
-
-				gui->AddDialogBox(tmpRoot.get("id", -1).asInt(), box);
-
-				thisFile.close();
 			}
 		}
 		}
