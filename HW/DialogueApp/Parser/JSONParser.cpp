b426af019ea0cc47f48d4b8916e3a7685761b6cc
ShaiMaytav <shaimaytav01@gmail.com>
2023-02-16 13:08:27 +0200

    Added required files and folders + implemented parser

--- HW/DialogueApp/Parser/JSONParser.cpp
+++ HW/DialogueApp/Parser/JSONParser.cpp
@@ -1,29 +1,56 @@
 #include "JSONParser.h"
+#include <iostream>
 
 void JSONParser::SetIndexFilePath(const std::string& _indexFilePath)
 {
+	indexFilePath = _indexFilePath;
 }
 
 const std::string& JSONParser::GetIndexFilePath() const
 {
-	return "Text";
+	return indexFilePath;
 }
 
 std::vector<std::string> JSONParser::GetData()
 {
-	return std::vector<std::string>();
+	std::vector<std::string> keys;
+	for (auto key : root)
+	{
+		keys.push_back(key.asString());
+	}
+	return keys;
 }
 
 bool JSONParser::Parse()
 {
-	return false;
+	if (!inputFile.is_open())
+	{
+		return false; 
+	}
+
+	bool parsingSuccessful = reader.parse(inputFile, root);
+
+	return parsingSuccessful;
 }
 
 bool JSONParser::OpenFile(const std::string& FilePath)
 {
-	return false;
+	CloseFile(); 
+
+	inputFile.open(FilePath, std::ifstream::binary); 
+
+	if (!inputFile.is_open())
+	{
+		std::cerr << "Failed to open file: " << FilePath << std::endl;
+		return false; 
+	}
+
+	indexFilePath = FilePath; 
+
+	return true; 
 }
 
 void JSONParser::CloseFile()
 {
+	CloseFile();
 }
