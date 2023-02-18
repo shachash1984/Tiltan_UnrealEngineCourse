a548dc78ee9d89b4745f51a28e5a99ff7783c6dd
nezarlawen1 <43337135+nezarlawen1@users.noreply.github.com>
2023-02-18 23:57:06 +0200

    Start Final Project

--- FinalProject/DialogApp/Parser/JSONParser.cpp
+++ FinalProject/DialogApp/Parser/JSONParser.cpp
@@ -2,27 +2,27 @@
 
 void JSONParser::SetIndexFilePath(const std::string& _indexFilePath)
 {
-	indexFilePath = _indexFilePath;
+    indexFilePath = _indexFilePath;
 }
 
 const std::string& JSONParser::GetIndexFilePath() const
 {
-	return indexFilePath;
+    return indexFilePath;
 }
 
 std::vector<std::string> JSONParser::GetData()
 {
-	return std::vector<std::string>();
+    return std::vector<std::string>();
 }
 
 bool JSONParser::Parse()
 {
-	return false;
+    return false;
 }
 
 bool JSONParser::OpenFile(const std::string& FilePath)
 {
-	return false;
+    return false;
 }
 
 void JSONParser::CloseFile()
