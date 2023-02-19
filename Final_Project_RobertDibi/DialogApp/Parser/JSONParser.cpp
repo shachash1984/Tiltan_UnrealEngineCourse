1454ffe918dfdebbd0672cd206e3e87d66b2f097
RobbyRicch <87952183+RobbyRicch@users.noreply.github.com>
2023-02-20 01:57:27 +0200

    Dialog and Parser

---
+++ Final_Project_RobertDibi/DialogApp/Parser/JSONParser.cpp
@@ -0,0 +1,30 @@
+#include "JSONParser.h"
+
+void JSONParser::SetIndexFilePath(const std::string& _indexFilePath)
+{
+    indexFilePath = _indexFilePath;
+}
+
+const std::string& JSONParser::GetIndexFilePath() const
+{
+    return indexFilePath;
+}
+
+std::vector<std::string> JSONParser::GetData()
+{
+    return std::vector<std::string>();
+}
+
+bool JSONParser::Parse()
+{
+    return false;
+}
+
+bool JSONParser::OpenFile(const std::string& FilePath)
+{
+    return false;
+}
+
+void JSONParser::CloseFile()
+{
+}
