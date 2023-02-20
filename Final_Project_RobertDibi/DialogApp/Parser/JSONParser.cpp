739961a985e83ac8ba19a1bbceac3eec4196fb7e
RobbyRicch <87952183+RobbyRicch@users.noreply.github.com>
2023-02-20 23:02:47 +0200

    Final Project Complete

--- Final_Project_RobertDibi/DialogApp/Parser/JSONParser.cpp
+++ Final_Project_RobertDibi/DialogApp/Parser/JSONParser.cpp
@@ -1,30 +1,97 @@
-#include "JSONParser.h"
+#include "JSONParser.h"	
+
 
 void JSONParser::SetIndexFilePath(const std::string& _indexFilePath)
 {
-    indexFilePath = _indexFilePath;
+	indexFilePath = _indexFilePath;
 }
 
 const std::string& JSONParser::GetIndexFilePath() const
 {
-    return indexFilePath;
+	return indexFilePath;
 }
 
 std::vector<std::string> JSONParser::GetData()
 {
-    return std::vector<std::string>();
+	return data;
 }
 
 bool JSONParser::Parse()
 {
-    return false;
+	//parsing the index file
+	if (inputFile.is_open())
+	{
+		reader.parse(inputFile, root);
+		if (!root.empty())
+		{
+			data.clear();
+
+			for (Json::Value::iterator itr = root.begin(); itr != root.end(); ++itr)
+			{
+				data.push_back(itr.key().asString());
+				//cout << itr.key().asString() << endl;
+
+			}
+
+			return true;
+		}
+	}
+
+	return false;
 }
 
 bool JSONParser::OpenFile(const std::string& FilePath)
 {
-    return false;
+	inputFile.open(FilePath);
+	if (!inputFile.is_open())
+	{
+		cout << "File Not Created" << endl;
+		return false;
+	}
+	else
+	{
+		cout << "File created successfuly!" << endl;
+		return true;
+	}
+
 }
 
 void JSONParser::CloseFile()
 {
+	cout << "Closeing File" << endl;
+	inputFile.close();
+	if (inputFile)
+	{
+		cout << "still have value" << endl;
+	}
+	else
+	{
+		cout << "Discarded value" << endl;
+	}
+
+}
+
+bool JSONParser::TryParseJSON(string filePath, bool printContent)
+{
+	SetIndexFilePath(filePath);
+	cout << GetIndexFilePath() << endl;
+
+	if (!OpenFile(GetIndexFilePath()))
+	{
+		cout << "File couldn't open" << endl;
+		return false;
+	}
+
+	Parse();
+	if (printContent)
+	{
+		cout << "\n---- " + filePath + " ----" << endl;
+		for (int i = 0; i < data.size(); i++)
+		{
+			cout << data[i] << endl;
+		}
+	}
+	CloseFile();
+
+	return true;
 }
--- Final_Project_RobertDibi/DialogApp/Parser/JSONParser.h
+++ Final_Project_RobertDibi/DialogApp/Parser/JSONParser.h
@@ -1,22 +1,28 @@
 #pragma once
-#include "json/json.h"
+#include <json/json.h>
 #include "IParser.h"
 #include <fstream>
+#include <iostream>
+using namespace std;
 
 class JSONParser : public IParser
 {
 public:
 	void SetIndexFilePath(const std::string& _indexFilePath);
 	const std::string& GetIndexFilePath() const;
-	std::vector<std::string> GetData() override;
-	bool Parse();
-	Json::Value& GetRoot() { return root; }
-	bool OpenFile(const std::string& FilePath);
-	void CloseFile();
 
+	std::vector<std::string> GetData() override; // Used to load the keys of the json index file into the data variable (from IParser) and return it
+
+	bool Parse(); // Used as a wrapper function for the user of this library to parse json files
+	Json::Value& GetRoot() { return root; }
+	bool OpenFile(const std::string& FilePath); //Used as a wrapper function for the user of this library to open a file (without using std::fstream)
+	void CloseFile(); //Same as Open but for closing the file
+	bool TryParseJSON(string filePath, bool printContent = false);
 protected:
-	std::string indexFilePath;
-	Json::Reader reader;
+	std::string indexFilePath; //Used to save the path to the JSON index file (the JSON file that contains the paths of all the dialog files)
+	Json::Reader reader;	// Used to read JSON files, see example in mainJSON2.cpp and also below on line 28
 	Json::Value root;   // 'root' will contain the root value after parsing.
-	std::ifstream inputFile;
+	std::ifstream inputFile; //CPP class for handling a file like we learned on class 10
 };
+
+
