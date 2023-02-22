6cde601cda9688d7baceebcb86e4940aebb047d4
Eli Zaidman-PC <Zaidmaneli@gmail.com>
2023-02-22 22:56:39 +0200

    Revert "Revert "Open only if desperate.png""
    This reverts commit 62e3721a7d04acb0162ec36fea57b9bdd14fe0ec.

--- Cpp_FinalProject/Cpp_DialogThingy/DialogElementSize.cpp
+++
@@ -1,8 +0,0 @@
-#include "DialogElementSize.h"
-
-DialogElementSize::DialogElementSize(std::shared_ptr<IGUI> const& pGui, int _width, int _height) : IDialogCreationElement(pGui), width(_width), height(_height) {}
-DialogElementSize::~DialogElementSize() {}
-void DialogElementSize::Act()
-{
-	gui->SetDialogSize(width, height);
-}
--- Cpp_FinalProject/Cpp_DialogThingy/DialogElementWindowColor.cpp
+++
@@ -1,8 +0,0 @@
-#include "DialogElementWindowColor.h"
-
-DialogElementWindowColor::DialogElementWindowColor(std::shared_ptr<IGUI> const& pGui, IGUI::Color _color) : IDialogCreationElement(pGui), color(_color) {}
-DialogElementWindowColor::~DialogElementWindowColor() {}
-void DialogElementWindowColor::Act()
-{
-	gui->SetWindowColor(color);
-}
