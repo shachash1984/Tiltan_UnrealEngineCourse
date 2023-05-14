169b0fba597336b2abb0db51f62d2e84d7e2dc35
benpanski <benpanski@gmail.com>
2023-05-14 11:14:00 +0300

    Revert "."
    This reverts commit 4078091f1163e6ea10b6dbaf636859b6c040b5bf.

--- HW/HW7/CPP7/CPP7/CPP7/Player.cpp
+++
@@ -1,7 +0,0 @@
-#include "Player.h"
-
-Player::Player() : _name("Person"), _playerBody(nullptr), _playerWeapon(nullptr) {}
-
-Player::Player(string name, Mesh* playerBody, Mesh* playerWeapon) : _name(name), _playerBody(playerBody), _playerWeapon(playerWeapon) {}
-
-Player::~Player() {}
